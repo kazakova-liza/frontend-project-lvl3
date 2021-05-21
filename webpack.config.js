fad104f1f2a69a44264a05f0921f31b586853f42
Liza Kazakova <kazakova.uk@gmail.com>
2021-05-21 19:31:51 +0100

    webpack configuration changed

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'dist/index.html',
+            template: 'public/index.html',
         }),
     ],
 };
