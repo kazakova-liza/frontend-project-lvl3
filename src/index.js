03ed2a0db97dd37fe248643a55682c454366ee44
Liza Kazakova <kazakova.uk@gmail.com>
2021-05-21 19:40:19 +0100

    added test area and button

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import 'bootstrap/dist/js/bootstrap.js'
 function component() {
     const element = document.createElement('div');
 
-    element.innerHTML = `Hi!`;
+    element.innerHTML = `<form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">RSS stream</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name">
+            <button type="submit" class="btn btn-primary">Add</button>
+        </div>
+        </form>`;
 
     return element;
 }
