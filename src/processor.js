ca9b9864f9f27aa0d43c8235cbed462391915406
Elizaveta Kazakova <kazakova.uk@gmail.com>
2021-12-14 10:34:33 +0000

    added url logging

--- src/processor.js
+++ src/processor.js
@@ -6,23 +6,28 @@ import saveRSS from './saver.js'
 
 
 const processRss = (url, newFlag, watchedState, i18nextInstance, schema) => {
+    const form = document.getElementsByClassName('form-control')[0];
     validate(url, schema)
         .then(() => {
+            console.log(form.value);
             if (newFlag) {
                 if (watchedState.feeds.find((feed) => feed.url === url) !== undefined) {
-                    console.log(url, 'duplicate');
+                    console.log(form.value);
                     throw (i18nextInstance.t('duplicate'));
                 }
             }
         })
         .then(() => getRSS(url))
         .then((response) => {
+            console.log(form.value);
             const parsedRSS = parse(response.data.contents);
             saveRSS(parsedRSS, url, newFlag, watchedState, i18nextInstance, schema);
-            const form = document.getElementsByClassName('form-control')[0];
+            // const form = document.getElementsByClassName('form-control')[0];
             form.value = '';
+            console.log(form.value);
         })
         .catch((err) => {
+            console.log(form.value);
             watchedState.valid = false;
             watchedState.validFeedback = '';
             if (err.errors !== undefined) {
