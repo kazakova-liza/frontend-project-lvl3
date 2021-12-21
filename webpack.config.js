171f12317828b26602ce69ca8868672846602195
Elizaveta Kazakova <kazakova.uk@gmail.com>
2021-12-21 19:43:10 +0000

    renaming

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/init.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'index_bundle.js',
