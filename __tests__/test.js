27835a620f77a94b950ff911ae96de558344619b
Elizaveta Kazakova <kazakova.uk@gmail.com>
2022-04-19 16:46:02 +0100

    changed eslint config and fixed linter issues

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,17 +1,15 @@
 // @ts-check
 
 import { promises as fs } from 'fs';
-import path from 'path';
-import init from '../src/init.js';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import init from '../src/init.js';
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const thisDirname = dirname(filename);
 
 beforeEach(async () => {
-  const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
+  const pathToHtml = path.resolve(thisDirname, '__fixtures__/index.html');
   const html = await fs.readFile(pathToHtml, 'utf8');
   document.body.innerHTML = html;
 });
