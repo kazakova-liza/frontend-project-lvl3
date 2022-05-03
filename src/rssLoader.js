f077c68090a7bbe46046ed9036cb50cedbcc2368
Elizaveta Kazakova <kazakova.uk@gmail.com>
2022-05-03 15:31:30 +0100

    added new URL to rssLoader.js

--- src/rssLoader.js
+++ src/rssLoader.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const proxifyUrl = (url) => `https://allorigins.hexlet.app/get?disableCache=true&url=${url}`;
 
 const getRSS = (url, i18nextInstance) => {
-  const proxifiedUrl = proxifyUrl(url);
+  const proxifiedUrl = new URL('https://allorigins.hexlet.app/get?disableCache=true&url=', url);
   return axios.get(proxifiedUrl).catch(() => {
     throw (i18nextInstance.t('networkError'));
   });
