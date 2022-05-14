283d274ba530e0b25ba94d6ee53711077ac5c8f4
Elizaveta Kazakova <kazakova.uk@gmail.com>
2022-05-14 10:14:42 +0100

    linter issue fix

--- src/rssLoader.js
+++ src/rssLoader.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getRSS = (url) => {
-  const baseURL = new URL(`https://allorigins.hexlet.app/get`);
+  const baseURL = new URL('https://allorigins.hexlet.app/get');
   baseURL.searchParams.append('disableCache', 'true').append('url', url);
   return axios.get(baseURL);
 };
