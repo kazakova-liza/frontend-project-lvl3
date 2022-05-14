3c50a348334bdb712dda81a1373cb8761f292a38
Elizaveta Kazakova <kazakova.uk@gmail.com>
2022-05-14 16:23:44 +0100

    getRSS fix

--- src/rssLoader.js
+++ src/rssLoader.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const getRSS = (url) => {
   const baseURL = new URL('https://allorigins.hexlet.app/get');
   baseURL.searchParams.append('disableCache', 'true').append('url', url);
+  baseURL.searchParams.append('url', url);
   return axios.get(baseURL);
 };
 
