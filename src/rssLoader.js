3d5d4717904e58ba47dcbf2117c6ebd65d9929d0
Elizaveta Kazakova <kazakova.uk@gmail.com>
2022-05-15 13:58:26 +0100

    base url fix

--- src/rssLoader.js
+++ src/rssLoader.js
@@ -4,7 +4,8 @@ const getRSS = (url) => {
   const baseURL = new URL('https://allorigins.hexlet.app/get');
   baseURL.searchParams.append('disableCache', 'true');
   baseURL.searchParams.append('url', url);
-  return axios.get(baseURL);
+  console.log(baseURL);
+  return axios.get(baseURL.href);
 };
 
 export default getRSS;
