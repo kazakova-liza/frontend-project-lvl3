0b39093f003fc025ad52129efc43992f4206b03e
Elizaveta Kazakova <kazakova.uk@gmail.com>
2022-05-15 14:22:39 +0100

    added name = "url" to the url input element

--- src/rssLoader.js
+++ src/rssLoader.js
@@ -4,7 +4,7 @@ const getRSS = (url) => {
   const baseURL = new URL('https://allorigins.hexlet.app/get');
   baseURL.searchParams.append('disableCache', 'true');
   baseURL.searchParams.append('url', url);
-  console.log(baseURL);
+  console.log(baseURL.href);
   return axios.get(baseURL.href);
 };
 
