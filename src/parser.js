8e7b73121759126d4265b1a2351fd9f69c016302
Elizaveta Kazakova <kazakova.uk@gmail.com>
2022-05-16 16:08:45 +0100

    changed DOM selectors in parser.js

--- src/parser.js
+++ src/parser.js
@@ -6,26 +6,25 @@ const parse = (xmlString, i18nextInstance) => {
   if (parserError) {
     throw (i18nextInstance.t('invalidRss'));
   }
-  const title = parsedString.querySelector('title');
-  const description = parsedString.querySelector('description');
+  const title = parsedString.querySelector('title').textContent;
+  const description = parsedString.querySelector('description').textContent;
   const items = [...parsedString.getElementsByTagName('item')];
 
   const posts = items.map((item) => {
-    const postTitle = item.getElementsByTagName('title')[0].textContent;
-    const link = item.getElementsByTagName('link')[0].textContent;
-    const postDescription = item.getElementsByTagName('description')[0].textContent;
-    const post = {
+    const postTitle = item.querySelector('title').textContent;
+    const link = item.querySelector('link').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    return {
       title: postTitle,
       link,
       description: postDescription,
       viewed: false,
     };
-    return post;
   });
 
   return {
-    title: title.textContent,
-    description: description.textContent,
+    title,
+    description,
     posts,
   };
 };
